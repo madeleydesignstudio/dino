94ab20fe9d8cbb2c33090fd6759c144da857902a
Dan <dev@madeleydesignstudio.org>
2024-11-02 10:06:18 +0000

    restyled to suit all devices

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -1,57 +1,58 @@
 import Image from "next/image";
 import Link from "next/link";
-import HyperText from "../ui/hyper-text";
 
 export default function LandingPage() {
   return (
-    <section className="flex">
-      <div className="relative h-screen">
+    <section className="hidden md:flex min-w-[768px] w-[125vw] relative">
+      {/* Photo background (covers entire section) */}
+      <div className="absolute inset-0">
+        <div className="relative h-full w-full">
+          <Image
+            src="https://ik.imagekit.io/danielcmadeley/dino/mxdeley_httpss.mj.runmHocz22p-iQ_Rick_and_morty_styled_pixel__95d55bd2-f449-4bc4-9ea9-ade80d3360c9_0.png?updatedAt=1729106463251"
+            alt="Desert landscape"
+            layout="fill"
+            objectFit="cover"
+            className="opacity-50"
+          />
+          <div className="absolute inset-0 bg-stone-50/30" />
+        </div>
+      </div>
+
+      <div className="relative h-screen flex-1">
         {/* White background (1/3 width) */}
         <div className="absolute left-0 top-0 h-full w-1/3 bg-white border-r border-stone-900" />
 
-        {/* Photo background (2/3 width) */}
-        <div className="absolute left-1/3 top-0 h-full w-full">
-          <div className="relative h-full w-full">
-            <Image
-              src="https://ik.imagekit.io/danielcmadeley/dino/mxdeley_httpss.mj.runmHocz22p-iQ_Rick_and_morty_styled_pixel__95d55bd2-f449-4bc4-9ea9-ade80d3360c9_0.png?updatedAt=1729106463251"
-              alt="Desert landscape"
-              layout="fill"
-              objectFit="cover"
-              className="opacity-50"
-            />
-            <div className="absolute inset-0 bg-stone-50/30" />{" "}
-          </div>
-        </div>
-
-        <div className="relative z-10 flex h-full w-screen -mr-6 flex-col justify-between">
-          <main className="">
-            <h2 className="pt-[1.5rem] text-[10.41rem] font-nanum pr-12 pl-6 text-center">
-              <HyperText text="Architects of the web." className="font-nanum" />
-            </h2>
-            <div className=" border-b border-stone-900 mr-6"></div>
-            <p className="text-7xl font-nanum pl-[34%] max-w-[65%] pt-6">
+        <div className="relative z-10 flex h-full w-full flex-col justify-between">
+          <main>
+            <div className="relative left-0  h-[33.333vh] border-b border-stone-900 flex items-center justify-center">
+              <h2 className="text-[9vw] font-nanum  text-center">
+                Architects of the web.
+              </h2>
+            </div>
+            <p className="text-[4vw] font-nanum pl-[34%] max-w-[70%] pt-6">
               &quot;Big agency thinking, small agency attitude.&quot;
             </p>
           </main>
 
-          <footer className="flex items-center justify-between">
-            <div className="flex">
-              <p className="font-bold text-7xl font-karla">DINO</p>
-              <div className="flex flex-col">
+          <footer className="flex items-center justify-between p-6">
+            <div className="flex items-start gap-2">
+              <p className="font-bold text-[4vw] font-karla">DINO</p>
+              <div className="flex flex-col pt-2">
                 <p className="text-xs font-karla">51.5072° N, 0.1276° W</p>
                 <p className="text-xs font-karla">UK DESIGN STUDIO</p>
               </div>
             </div>
-            <p className="text-4xl mr-12 font-nanum">
+            <p className="text-[2vw] font-nanum">
               Scroll <span className="ml-2">→</span>
             </p>
           </footer>
         </div>
       </div>
-      <div className=" w-[568px] h-screen border-r border-l border-stone-900 z-10">
+
+      <div className="w-[30vw] max-w-[568px] min-w-[400px] h-screen border-r border-l border-stone-900 z-10">
         <Link href="/services/design">
-          <div className="h-1/3 flex flex-col justify-end pb-6 px-6 border-b border-stone-900 hover:bg-stone-200/50">
-            <h2 className="text-7xl  mb-2 font-nanum">Design</h2>
+          <div className="flex-1 h-1/3 flex flex-col justify-end pb-6 px-6 border-b border-stone-900 hover:bg-stone-200/50">
+            <h2 className="text-[4vw] mb-2 font-nanum">Design</h2>
             <p className="text-xs">
               Elevate your digital experience and delight every user. It all
               starts here.
@@ -60,8 +61,8 @@ export default function LandingPage() {
         </Link>
 
         <Link href="/services/engineer">
-          <div className="h-1/3 flex flex-col justify-end pb-6 px-6 border-b border-stone-900 hover:bg-stone-200/50">
-            <h2 className="text-7xl  mb-2 font-nanum">Engineer</h2>
+          <div className="flex-1 h-1/3 flex flex-col justify-end pb-6 px-6 border-b border-stone-900 hover:bg-stone-200/50">
+            <h2 className="text-[4vw] mb-2 font-nanum">Engineer</h2>
             <p className="text-xs">
               Make zero comprises when implementing your vision, using a
               powerful tech stack.
@@ -70,8 +71,8 @@ export default function LandingPage() {
         </Link>
 
         <Link href="/services/market">
-          <div className="h-1/3 flex flex-col justify-end pb-6 px-6 border-b border-stone-900 hover:bg-stone-200/50">
-            <h2 className="text-7xl  mb-2 font-nanum">Market</h2>
+          <div className="flex-1 h-1/3 flex flex-col justify-end pb-6 px-6 hover:bg-stone-200/50">
+            <h2 className="text-[4vw] mb-2 font-nanum">Market</h2>
             <p className="text-xs">
               Your deadline is our deadline. Your success is our success.
             </p>
