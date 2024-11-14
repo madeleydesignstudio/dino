38db580c7ee442eb15d1ea53305198ce6f4fdbf2
Dan <dev@madeleydesignstudio.org>
2024-11-14 08:10:53 +0000

    added bg

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -15,14 +15,14 @@ export default function LandingPage() {
             objectFit="cover"
             className="opacity-50 "
           />
-          <div className="absolute inset-0 bg-gradient-to-t from-stone-50/10 to-stone-50/30 dark:from-stone-950/60 dark:to-stone-950/30 backdrop-blur-sm" />
+          <div className="absolute inset-0 bg-gradient-to-t from-stone-50/10 to-stone-50/30 dark:from-stone-900/60 dark:to-stone-900/30 backdrop-blur-sm" />
         </div>
       </div>
 
       <div className="relative h-screen flex-1">
         {/* White background (1/3 width) */}
         {/* <div className="absolute left-0 top-0 h-full w-1/3 backdrop-blur-xl bg-stone-50/80 dark:bg-stone-950/80 border-r border-stone-900 dark:border-stone-300"/> */}
-        <AuroraBackground className="absolute left-0 top-0 h-full w-1/3 backdrop-blur-xl bg-stone-50/80 dark:bg-stone-950/80 border-r border-stone-900 dark:border-stone-300">
+        <AuroraBackground className="absolute left-0 top-0 h-full w-1/3 backdrop-blur-xl bg-stone-50/80 dark:bg-stone-950/80 border-r border-stone-900 dark:border-stone-400">
           <motion.div
             initial={{ opacity: 0.0, y: 40 }}
             whileInView={{ opacity: 1, y: 0 }}
@@ -37,7 +37,7 @@ export default function LandingPage() {
 
         <div className="relative z-10 flex h-full w-full flex-col justify-between">
           <main>
-            <div className="relative left-0  h-[33.333vh] border-b border-stone-900 dark:border-stone-300 flex items-center justify-center">
+            <div className="relative left-0  h-[33.333vh] border-b border-stone-900 dark:border-stone-400 flex items-center justify-center">
               <h2 className="text-[9vw] font-nanum  text-center">
                 Architects of the web.
               </h2>
@@ -63,9 +63,9 @@ export default function LandingPage() {
         </div>
       </div>
 
-      <div className="w-[30vw] max-w-[568px] min-w-[400px] h-screen border-r border-l border-stone-900 dark:border-stone-300 z-10">
+      <div className="w-[30vw] max-w-[568px] min-w-[400px] h-screen border-r border-l border-stone-900 dark:border-stone-400 z-10">
         <a href="/services/design">
-          <div className="flex-1 h-1/3 flex flex-col justify-end pb-6 px-6 border-b border-stone-900 dark:border-stone-300 hover:bg-stone-200/50 transition-colors dark:hover:bg-stone-800/50">
+          <div className="flex-1 h-1/3 flex flex-col justify-end pb-6 px-6 border-b border-stone-900 dark:border-stone-400 hover:bg-stone-200/50 transition-colors dark:hover:bg-stone-800/50">
             <h2 className="text-[4vw] mb-2 font-nanum">Design</h2>
             <p className="text-xs">
               Elevate your digital experience and delight every user. It all
@@ -75,7 +75,7 @@ export default function LandingPage() {
         </a>
 
         <a href="/services/engineer">
-          <div className="flex-1 h-1/3 flex flex-col justify-end pb-6 px-6 border-b border-stone-900 dark:border-stone-300 hover:bg-stone-200/50 transition-colors dark:hover:bg-stone-800/50">
+          <div className="flex-1 h-1/3 flex flex-col justify-end pb-6 px-6 border-b border-stone-900 dark:border-stone-400 hover:bg-stone-200/50 transition-colors dark:hover:bg-stone-800/50">
             <h2 className="text-[4vw] mb-2 font-nanum">Engineer</h2>
             <p className="text-xs">
               Make zero comprises when implementing your vision, using a
--- src/components/landing-page/vertical-footer.tsx
+++ src/components/landing-page/vertical-footer.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 export default function VerticalFooter() {
   return (
@@ -72,24 +73,37 @@ export default function VerticalFooter() {
             height={100}
           />
           <div className="flex space-x-8 mb-4">
-            <img
-              src="https://ik.imagekit.io/danielcmadeley/dino/x.svg?updatedAt=1729714447358"
-              alt="X"
-              width={20}
-              height={20}
-            />
-            <img
-              src="https://ik.imagekit.io/danielcmadeley/dino/github.svg?updatedAt=1729714431708"
-              alt="Github"
-              width={20}
-              height={20}
-            />
-            <img
-              src="https://ik.imagekit.io/danielcmadeley/dino/linkedin.svg?updatedAt=1729714466076"
-              alt="Linkedin"
-              width={20}
-              height={20}
-            />
+            <Link href="https://github.com/danielcmadeley">
+              <svg
+                width={20}
+                height={20}
+                viewBox="0 0 24 24"
+                className="fill-stone-900 dark:fill-stone-50"
+              >
+                <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z" />
+              </svg>
+            </Link>{" "}
+            <Link href="https://www.linkedin.com/in/danielcmadeley/">
+              {" "}
+              <svg
+                width={20}
+                height={20}
+                viewBox="0 0 24 24"
+                className="fill-stone-900 dark:fill-stone-50"
+              >
+                <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+              </svg>
+            </Link>
+            <Link href="https://www.x.com/danielcmadeley">
+              <svg
+                width={20}
+                height={20}
+                viewBox="0 0 24 24"
+                className="fill-stone-900 dark:fill-stone-50"
+              >
+                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+              </svg>
+            </Link>
           </div>
           <div className="text-xs">
             <p>51.5072° N, 0.1276° W</p>
@@ -110,7 +124,7 @@ export default function VerticalFooter() {
       <div className="absolute inset-y-0 right-0 flex items-center overflow-hidden pointer-events-none -z-10">
         <div className="relative left-1/2">
           <span
-            className="text-[25rem] font-bold text-black leading-none rotate-180"
+            className="text-[25rem] font-bold text-stone-900 dark:text-stone-50 leading-none rotate-180"
             style={{ writingMode: "vertical-rl", textOrientation: "mixed" }}
           >
             DINO
