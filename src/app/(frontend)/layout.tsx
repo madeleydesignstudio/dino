0b437692a948956e876b58f5c57c4fdabfb87eca
danielcmadeley <daniel@madeleydesignstudio.org>
2025-06-19 21:09:33 +0100

    added neon-db

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './styles.css'
+import { OpenPanelComponent } from '@openpanel/nextjs'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
@@ -18,6 +19,15 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
           data-website-id="50f0d6bf-596e-4e58-8e58-69aa5142ad2a"
         ></script>
       </head>
+
+      <OpenPanelComponent
+        clientId="6621cc22-a1ff-4225-a38e-cc0cec494e81"
+        trackScreenViews={true}
+        // trackAttributes={true}
+        // trackOutgoingLinks={true}
+        // If you have a user id, you can pass it here to identify the user
+        // profileId={'123'}
+      />
       <body>
         <main>{children}</main>
       </body>
