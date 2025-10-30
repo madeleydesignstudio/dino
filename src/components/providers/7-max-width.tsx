81450494a47fdfc9fbd5bc5c7166e173dac49526
danielcmadeley <daniel@madeleydesignstudio.org>
2025-10-30 21:02:19 +0000

    added three js

--- src/components/providers/7-max-width.tsx
+++ src/components/providers/7-max-width.tsx
@@ -1,4 +1,8 @@
-const SevenMaxWidth = ({ children }) => {
+interface SevenMaxWidthProps {
+  children: React.ReactNode;
+}
+
+const SevenMaxWidth = ({ children }: SevenMaxWidthProps) => {
   return (
     <div className="max-w-7xl mx-auto border-x border-[#D9E0C1]">
       {children}
