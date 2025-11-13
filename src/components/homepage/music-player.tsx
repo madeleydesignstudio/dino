8cb8b14e088dcdc58797a0de22aebd02044c535d
danielcmadeley <daniel@madeleydesignstudio.org>
2025-11-13 19:19:54 +0000

    adapted mobile styling

--- src/components/homepage/music-player.tsx
+++ src/components/homepage/music-player.tsx
@@ -1,12 +1,12 @@
-import { ServerImage } from "@/components/universal/ServerImage";
-import { MusicPlayerClient } from "./MusicPlayerClient";
+// import { ServerImage } from "@/components/universal/ServerImage";
+// import { MusicPlayerClient } from "./MusicPlayerClient";
 
-export const MusicPlayer = () => {
-  return (
-    <MusicPlayerClient
-      albumArt={
-        <ServerImage id={10} width={60} height={60} priority quality={100} />
-      }
-    />
-  );
-};
+// export const MusicPlayer = () => {
+//   return (
+//     <MusicPlayerClient
+//       albumArt={
+//         <ServerImage id={10} width={60} height={60} priority quality={100} />
+//       }
+//     />
+//   );
+// };
